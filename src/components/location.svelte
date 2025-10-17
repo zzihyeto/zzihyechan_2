211de6a970855d679b86a6a3c487531ed8c14a2d
zzihyeto <ljh9055@gmail.com>
2025-10-17 23:22:46 +0900

    251017

--- src/components/location.svelte
+++ src/components/location.svelte
@@ -24,8 +24,8 @@
 
 	function shareKakaoTalk() {
 		const url   = window.location.href;
-		const title = '임찬교와 이지혜의 결혼식에 초대합니다!';
-		const desc  = '26.01.31 (토) 오후 12시 함께 해주세요!';
+		const title = "ChanGyo & JiHye's Wedding";
+		const desc  = '💐26.01.31 (토) 오후 12시 함께 해주세요';
 		const image = 'https://zzihyechanweddinginvite.netlify.app/black_half_1.jpg'; // 가급적 1200x630 권장
 
 		// 1) Kakao 공식 공유 (PC/모바일 공통)
